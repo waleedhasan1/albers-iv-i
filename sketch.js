4dde15034ab04be5ca5e6d3b01ca258ff1804373
Waleed Hasan <31443623+waleedhasan1@users.noreply.github.com>
2022-02-09 16:44:15 -0600

    Update sketch.js
    changing colors back to default

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
-
 let clrs = [
-'#333333', //top 0
-'blue', //middle top 1
-'#828282', //middle strip 2
-'green', //middle bottom 3
-'#CCCCCC' //bottom 4
+'#E27F34', //top 0
+'#FEF254', //middle top 1
+'#A36C33', //middle strip 2
+'#042060', //middle bottom 3
+'#3778AE' //bottom 4
 ]
 
 
